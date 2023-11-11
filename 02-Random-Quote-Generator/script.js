022739a308f0d7714285065bea1550b97923902b
Mahedi <lazyboy518@yahoo.com>
2023-11-11 15:58:32 +0600

    code fixed

--- 02-Random-Quote-Generator/script.js
+++ 02-Random-Quote-Generator/script.js
@@ -4,11 +4,16 @@ const author = document.querySelector('.author');
 
 const API_URL = 'https://api.quotable.io/random';
 async function quoteGenerator(){
-    const res = await fetch(API_URL);
-    const data = await res.json();
-    console.log(data);
-    content.textContent = `"${data.content}"`;
-    author.textContent = `〰️ ${data.author}`;
+    
+    try {
+        const res = await fetch(API_URL);
+        const data = await res.json();
+        console.log(data);
+        content.textContent = `"${data.content}"`;
+        author.textContent = `〰️ ${data.author}`;
+    } catch (error) {
+        console.log(error);
+    }
 
 }
 
